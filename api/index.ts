52ef0817d4ae852478bc5f2394426b6b3538b047
Sonu Singhal <ssingha@bettr.me>
2021-08-26 00:23:30 +0530

    port changed
    port changed

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { getFeeds } from './routes/feeds';
 
 const app = express();
-const port = 3000;
+const port = 3005;
 
 /**
  * Get Feeds
--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3000',
+      target: 'http://localhost:3005',
       changeOrigin: true,
       pathRewrite: {
         '/api': '/'
